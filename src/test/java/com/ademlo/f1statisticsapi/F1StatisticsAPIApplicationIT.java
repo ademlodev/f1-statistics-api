c6b23618446446923c68e77845d4948f206874a8
Javier García <javier.garcia.olmeda@gmail.com>
2025-02-03 00:59:55 +0100

    test: set season get list all seasons

--- src/test/java/com/ademlo/f1statisticsapi/F1StatisticsAPIApplicationIT.java
+++ src/test/java/com/ademlo/f1statisticsapi/F1StatisticsAPIApplicationIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class F1StatisticsAPIApplicationTests {
+class F1StatisticsAPIApplicationIT {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/com/ademlo/f1statisticsapi/features/season/infra/http/SeasonControllerIT.java
@@ -0,0 +1,40 @@
+package com.ademlo.f1statisticsapi.features.season.infra.http;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Season Feature")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SeasonControllerIT {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+
+    @Test
+    @DisplayName("Given list season return all seasons")
+    void givenUserGetSeasonListThenReturnAll(){
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/season")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("$.size()", greaterThan(0))
+                .body("[0].year", not(emptyString()))
+                .body("[0].year", greaterThan(1949));
+    }
+}
